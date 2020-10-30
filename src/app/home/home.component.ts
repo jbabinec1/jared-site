841c36f034405c320beeb80b6046eb5ed02aacfa
Jared <jbabinec1@gmail.com>
2020-10-30 00:13:03 -0400

    responsive side bar

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, HostListener, Input } from '@angular/core';
 import {butterService} from './../Services/butterCMS.service';
+import { Observable, fromEvent } from 'rxjs';
+import { debounceTime, map, startWith } from 'rxjs/operators';
 
 
  
@@ -12,20 +14,35 @@ import {butterService} from './../Services/butterCMS.service';
 })
 export class HomeComponent implements OnInit {
 
-  closeNav()  {
+   //mq = window.matchMedia( "(min-width: 768px)" );
+
+  closeNav()  { 
     this.toggle = !this.toggle;
   } 
 
+
  public toggle: boolean; 
+
+
+public isScreenSmall$: Observable<any>;
 
   constructor(  ) { }
 
   
   
-
   ngOnInit() {
    
     this.toggle = false; 
+   
+   // Checks if screen size is less than 1024 pixels
+  const checkScreenSize = () => document.body.offsetWidth < 768;
+
+  // Create observable from window resize event throttled so only fires every 500ms
+  const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(500)).pipe(map(checkScreenSize));
+
+  // Start off with the initial value use the isScreenSmall$ | async in the
+  // view to get both the original value and the new value after resize.
+  this.isScreenSmall$ = screenSizeChanged$.pipe(startWith(checkScreenSize()))
     
   }
 
