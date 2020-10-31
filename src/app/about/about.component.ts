7f9276adc68b183b74d7fddc83f459ba66870775
Jared <jbabinec1@gmail.com>
2020-10-31 02:23:38 -0400

    add responsive side bar to about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import {HomeComponent} from 'src/app/home/home.component';
 import { WelcomeService } from '../Services/welcome.service';
 import {SidebarComponent} from 'src/app/sidebar/sidebar.component';
+import { fromEvent, Observable } from 'rxjs';
+import { debounceTime, map, startWith } from 'rxjs/operators';
 
 
 
@@ -16,8 +18,10 @@ import {SidebarComponent} from 'src/app/sidebar/sidebar.component';
   providers:[WelcomeService, HomeComponent]
 })
 export class AboutComponent implements OnInit {
+
   public toggle: boolean;
 
+  public isScreenSmall$: Observable<any>;
 
   constructor( ) { }
 
@@ -25,7 +29,19 @@ export class AboutComponent implements OnInit {
 
     this.toggle = false;
 
+  // Checks if screen size is less than 1024 pixels
+  const checkScreenSize = () => document.body.offsetWidth < 768;
+
+  // Create observable from window resize event throttled so only fires every 500ms
+  const screenSizeChanged$ = fromEvent(window, 'resize').pipe(debounceTime(500)).pipe(map(checkScreenSize));
+
+  // Start off with the initial value use the isScreenSmall$ | async in the
+  // view to get both the original value and the new value after resize.
+  this.isScreenSmall$ = screenSizeChanged$.pipe(startWith(checkScreenSize()))
+
   }
+
+  
 
   closeNavy() {
     this.toggle = !this.toggle;
